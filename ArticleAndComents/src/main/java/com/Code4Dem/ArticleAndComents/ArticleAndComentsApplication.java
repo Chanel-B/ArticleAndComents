19828c18f4984116965fdfb33540462476c733d6
Chanel-B <84045976+Chanel-B@users.noreply.github.com>
2022-03-31 15:30:23 +0200

    Initialized commit

---
+++ ArticleAndComents/src/main/java/com/Code4Dem/ArticleAndComents/ArticleAndComentsApplication.java
@@ -0,0 +1,13 @@
+package com.Code4Dem.ArticleAndComents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleAndComentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleAndComentsApplication.class, args);
+	}
+
+}
---
+++ ArticleAndComents/src/test/java/com/Code4Dem/ArticleAndComents/ArticleAndComentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Code4Dem.ArticleAndComents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleAndComentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
